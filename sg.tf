5d14c8ed57c19db7c4bb18f57ff6ccb3adcd586c
InbarHavdala <129771643+InbarHavdala@users.noreply.github.com>
2024-06-16 20:40:56 +0300

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "alb-sg" {
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+resource "aws_security_group" "ecs_service" {
+  description = "Allow HTTP traffic to ECS tasks"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80 
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-service-sg"
+  }
+}
